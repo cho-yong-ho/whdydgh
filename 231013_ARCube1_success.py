6c7ab98c2f2611abed2e564750efbc4daaded140
whdydgh <115132624+cho-yong-ho@users.noreply.github.com>
2023-10-15 17:37:02 +0900

    Update 231013_ARCube1_success.py

--- 231013_ARCube1_success.py
+++ 231013_ARCube1_success.py
@@ -1,13 +1,15 @@
 import cv2
 import numpy as np
 
+# 카메라 해상도 설정
+width = 640
+height = 480
+
 # 체커보드의 가로 및 세로 꼭지점 수 설정
 rows = 4
 cols = 7
 
-# 카메라 해상도 설정
-width = 640  # 원하는 너비로 설정
-height = 480  # 원하는 높이로 설정
+axis_length = rows-1  # 큐브 모서리의 길이
 
 # 체커보드의 꼭지점 좌표 생성
 objp = np.zeros((rows * cols, 3), np.float32)
@@ -20,24 +22,26 @@ imgpoints = []  # 2D 이미지 상의 꼭지점 좌표
 def draw_cube(img, corners, imgpts):
     imgpts = np.int32(imgpts).reshape(-1, 2)
     
-    # 큐브 그리기
+    # 큐브 아랫면 그리기
     for i in range(4):
-        img = cv2.line(img, tuple(imgpts[i]), tuple(imgpts[(i+1) % 4]), (0, 0, 255), 3)
-    img = cv2.line(img, tuple(imgpts[0]), tuple(imgpts[4]), (0, 0, 255), 3)
-    img = cv2.line(img, tuple(imgpts[1]), tuple(imgpts[5]), (0, 0, 255), 3)
-    img = cv2.line(img, tuple(imgpts[2]), tuple(imgpts[6]), (0, 0, 255), 3)
-    img = cv2.line(img, tuple(imgpts[3]), tuple(imgpts[7]), (0, 0, 255), 3)
+        img = cv2.line(img, tuple(imgpts[i]), tuple(imgpts[(i+1) % 4]), (0, 0, 255), 1)
 
-    # 바닥면 그리기
-    img = cv2.line(img, tuple(imgpts[4]), tuple(imgpts[5]), (0, 0, 255), 3)
-    img = cv2.line(img, tuple(imgpts[5]), tuple(imgpts[6]), (0, 0, 255), 3)
-    img = cv2.line(img, tuple(imgpts[6]), tuple(imgpts[7]), (0, 0, 255), 3)
-    img = cv2.line(img, tuple(imgpts[7]), tuple(imgpts[4]), (0, 0, 255), 3)
+    # 큐브 측면 모서리 그리기
+    img = cv2.line(img, tuple(imgpts[0]), tuple(imgpts[4]), (0, 0, 255), 1)
+    img = cv2.line(img, tuple(imgpts[1]), tuple(imgpts[5]), (0, 0, 255), 1)
+    img = cv2.line(img, tuple(imgpts[2]), tuple(imgpts[6]), (0, 0, 255), 1)
+    img = cv2.line(img, tuple(imgpts[3]), tuple(imgpts[7]), (0, 0, 255), 1)
+
+    # 윗면 그리기
+    img = cv2.line(img, tuple(imgpts[4]), tuple(imgpts[5]), (0, 0, 255), 1)
+    img = cv2.line(img, tuple(imgpts[5]), tuple(imgpts[6]), (0, 0, 255), 1)
+    img = cv2.line(img, tuple(imgpts[6]), tuple(imgpts[7]), (0, 0, 255), 1)
+    img = cv2.line(img, tuple(imgpts[7]), tuple(imgpts[4]), (0, 0, 255), 1)
 
     return img
 
-# 비디오 캡처 또는 이미지 불러오기
-cap = cv2.VideoCapture(0)  # 카메라를 사용하려면 0을 사용하거나 동영상 파일 경로를 지정할 수 있습니다.
+# 웹캠 사용
+cap = cv2.VideoCapture(0)
 
 # 카메라 해상도 설정
 cap.set(3, width)  # 너비 설정
@@ -49,35 +53,23 @@ while True:
     if not ret:
         break
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     # 체커보드 검출
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     ret, corners = cv2.findChessboardCorners(gray, (cols, rows), None)
 
     if ret:
+        # 체스보드 꼭지점을 일정 수준 이상의 정확도로 탐지
         criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
         corners = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
         
+        # 각 리스트에 체스보드 꼭지점 좌표 추가
         objpoints.append(objp)
         imgpoints.append(corners)
 
         # 카메라 캘리브레이션 실행
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-        print('mtx')
-        print(mtx)
-        print(type(mtx))
-        print('dist')
-        print(dist)
-        print(type(dist))
-        print('rvecs')
-        print(rvecs)
-        print(type(rvecs))
-        print('tvecs')
-        print(tvecs)
-        print(type(tvecs))
-
+     
         # 3D 큐브 좌표 설정
-        axis_length = rows-1  # 큐브의 변 길이
         axis_points = np.float32([[0, 0, 0], [0, axis_length, 0], [axis_length, axis_length, 0], [axis_length, 0, 0], [0, 0, -axis_length], [0, axis_length, -axis_length], [axis_length, axis_length, -axis_length], [axis_length, 0, -axis_length]])
 
         # 카메라의 위치와 방향 추정 (3D 큐브 위치 결정)
@@ -88,9 +80,11 @@ while True:
 
         # 3D 큐브 그리기
         frame = draw_cube(frame, corners, imgpts)
-
+    
+    # 화면 출력
     cv2.imshow('Checkerboard Detection', frame)
 
+    # q를 누르면 프로그램 종료
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
